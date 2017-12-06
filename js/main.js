c59ec6d4f0837a9efce251980418b7d0db109b4a
ycylingithub <ycylmail@qq.com>
2017-12-06 16:20:19 +0800

    npm打包
    将js文件和css文件打包

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true
-    });
-});
